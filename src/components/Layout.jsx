b2d49db8e1c5b2698994684215c981c9e6e3830d
Davide Gennaro <davidegennaro96@gmail.com>
2026-05-20 17:42:18 +0200

    created Navbar component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
 
 
 export default function Layout () {
     return (
         <>
+            <Navbar />
             <Outlet />
         </>
     )
